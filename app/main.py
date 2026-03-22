0b5493c3d09965947d65cff3fd01c881e6237cd5
Seytaaa <seytaaa.contact@gmail.com>
2026-03-22 11:04:43 +0100

    refactor code so its more clear who parses and how to pointer goes onward in file

--- app/main.py
+++ app/main.py
@@ -3,10 +3,6 @@ from operator import invert
 from src.fetcher import fetch_block_hash
 from src.fetcher import fetch_raw_block
 from src.block import Block
-from src.transaction import Transaction
-from src.utils import read_varint
-from src.pow import check_pow
-from src.merkle import merkle_root
 from src.script import parse_script
 from src.address import hash_to_address
 import io
@@ -20,7 +16,6 @@ block_raw = fetch_raw_block(block_hash)
 block_hash = fetch_block_hash(500000)
 block_raw = fetch_raw_block(block_hash)
 
-print(block_raw)
 block_stream = io.BytesIO(block_raw)
 block = Block.parse(block_stream)
 
@@ -28,30 +23,14 @@ print(block.version)
 print(block.timestamp)
 print(block.nonce)
 
-transaction_count = read_varint(block_stream)
-
-transactions = []
-transaction_ids = []
-for i in range(transaction_count):
-    transaction = Transaction.parse(block_stream)
-    print(transaction.version)
-    print(transaction.segwit)
-    print(len(transaction.inputs))
-    print(len(transaction.outputs))
-
-    transactions.append(transaction)
-    transaction_ids.append(bytes.fromhex(transaction.transaction_id())[::-1])
-
-print('Pow')
-print(check_pow(block))
-
-merkle = merkle_root(transaction_ids)
-print(merkle)
-print(block.merkle_root)
-print(merkle == block.merkle_root)
+print('Checks')
+print(f'Pow {block.check_pow()}')
+print(f'Merkle {block.check_merkle_root()}')
 
 print('Address')
-for transaction in transactions:
+for transaction in block.transactions:
     for output in transaction.outputs:
         script_type, hash_bytes = parse_script(output.script_pubkey)
-        print(hash_to_address(script_type, hash_bytes))
+        address = (hash_to_address(script_type, hash_bytes))
+        amount = output.amount
+        print(f'{address} - {amount / 100_000_000} BTC')
--- app/src/block.py
+++ app/src/block.py
@@ -1,25 +1,42 @@
 from src.utils import little_endian_to_int
 from src.utils import int_to_little_endian
+from src.utils import read_varint
 from src.utils import hash256
+from src.transaction import Transaction
+from src.merkle import merkle_root
+from src.pow import check_pow
 
 class Block:
-    def __init__(self, version, prev_block, merkle_root, timestamp, bits, nonce):
+    def __init__(self, version, prev_block, merkle_root, timestamp, bits, nonce, transactions=None, transaction_ids=None):
         self.version = version
         self.prev_block = prev_block
         self.merkle_root = merkle_root
         self.timestamp = timestamp
         self.bits = bits
         self.nonce = nonce
+        self.transactions = transactions if transactions is not None else []
+        self.transaction_ids = transaction_ids if transaction_ids is not None else []
 
     @classmethod
     def parse(cls, stream):
+        #Block headers 80 bytes
         version = little_endian_to_int(stream.read(4))
         prev_block = stream.read(32)
         merkle_root = stream.read(32)
         timestamp = little_endian_to_int(stream.read(4))
         bits = little_endian_to_int(stream.read(4))
         nonce = little_endian_to_int(stream.read(4))
-        return cls(version, prev_block, merkle_root, timestamp, bits, nonce)
+
+        # number of transactions
+        transaction_count = read_varint(stream)
+        transactions = []
+        transaction_ids = []
+        for i in range(transaction_count):
+            transaction = Transaction.parse(stream)
+            transactions.append(transaction)
+            transaction_ids.append(bytes.fromhex(transaction.transaction_id())[::-1])
+
+        return cls(version, prev_block, merkle_root, timestamp, bits, nonce, transactions, transaction_ids)
 
     def hash(self):
         hash = hash256(
@@ -32,3 +49,9 @@ class Block:
         )
         inverted = hash[::-1].hex()
         return inverted
+
+    def check_merkle_root(self):
+        return merkle_root(self.transaction_ids)
+
+    def check_pow(self):
+        return check_pow(self)
